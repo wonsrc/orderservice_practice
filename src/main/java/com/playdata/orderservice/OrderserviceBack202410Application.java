c2750ef24106cb92ac5b3f3d9b4470f7dfc22d52
LeeKM321 <stephen4951@gmail.com>
2024-10-31 17:50:10 +0900

    initial commit

---
+++ src/main/java/com/playdata/orderservice/OrderserviceBack202410Application.java
@@ -0,0 +1,13 @@
+package com.playdata.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderserviceBack202410Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderserviceBack202410Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/playdata/orderservice/OrderserviceBack202410ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderserviceBack202410ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
