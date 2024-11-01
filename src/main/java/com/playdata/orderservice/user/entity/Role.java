717b23c957417a118fc31c6270fa9c516dda5c9d
LeeKM321 <stephen4951@gmail.com>
2024-11-01 12:02:19 +0900

    User 엔터티 생성

---
+++ src/main/java/com/playdata/orderservice/user/entity/Role.java
@@ -0,0 +1,5 @@
+package com.playdata.orderservice.user.entity;
+
+public enum Role {
+    USER, ADMIN
+}
---
+++ src/main/java/com/playdata/orderservice/user/entity/User.java
@@ -0,0 +1,53 @@
+package com.playdata.orderservice.user.entity;
+
+import com.playdata.orderservice.common.entity.Address;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter @NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Embedded // @Embeddable로 선언된 값 대입 (기본 생성자 필수)
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default // build시 초기화된 값으로 세팅하기 위한 아노테이션
+    private Role role = Role.USER;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
